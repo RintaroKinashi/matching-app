04b26fb9ec9e2edaeae62166b4f5eae5d6fc98d5
KinashiRintaro <nagisa624174@gmail.com>
2021-07-19 12:49:36 +0900

    Add 分析画面

--- question.php
+++ question.php
@@ -11,6 +11,42 @@ $dsn='mysql:dbname=EC;charset=utf8';
 $user='root';
 $password='';
 $dbh = new PDO($dsn,$user,$password);
+
+// 問題数
+const NUMBER_OF_QUESTIONS = 3;
+
+$stmt1 = $dbh->prepare("SELECT * FROM r_answer WHERE UserID=:userID");
+$stmt1->bindParam(':userID', $_SESSION["login"]);
+$stmt1->execute();
+$result1 = $stmt1 -> fetch(PDO::FETCH_ASSOC);
+
+if(isset($_POST['q1'])){
+  if (empty($result1["UserID"])){
+    // IDが空ならinsert
+    $stmt = $dbh->prepare("INSERT INTO r_answer VALUES(
+        :userID,
+        :q1,
+        :q2,
+        :q3
+    )");
+  }
+  else{
+    $stmt = $dbh->prepare("UPDATE r_answer SET
+        q1 = :q1,
+        q2 = :q2,
+        q3 = :q3
+        WHERE UserID = :userID");
+  }
+    $stmt->bindParam(':userID', $_SESSION["login"]);
+    for ($i=1;$i<NUMBER_OF_QUESTIONS+1;$i++){
+      $stmt->bindParam(':q'. $i, $_POST['q'. $i]);
+    }
+    $stmt->execute();
+    header("Location: profile.php");
+    exit();
+}
+
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -30,11 +66,11 @@ $dbh = new PDO($dsn,$user,$password);
           <li><a href="./logout.php">ログアウト</a></li>
         </ul>
       </div>
-    </div>
+  </div>
 
 <form action="question.php" method="post">
 <?php
-  for ($i=1;$i<3;$i++){
+  for ($i=1;$i<NUMBER_OF_QUESTIONS+1;$i++){
   $stmtq = $dbh->prepare("SELECT * FROM m_question WHERE questionID=$i");
   $stmtq->execute();
   $resultq = $stmtq -> fetch(PDO::FETCH_ASSOC);
@@ -42,7 +78,9 @@ $dbh = new PDO($dsn,$user,$password);
     echo "<div class='question'>";
     echo "<p>" . $resultq["sentence"] . "</p>";
     for ($j=0;$j<4;$j++){
-      echo "<input type='radio' name='q2' value='" . $j ."'>" . $resultq["Choice".$j+1];
+      $k = "";
+      if($result1['q'. $i]==$j){$k = "checked";}
+      echo "<input type='radio' name='q". $i ."' value='" . $j ."' ". $k .">" . $resultq["Choice".$j+1]; // if(!empty($result1["UserID"])){if($result1['q'. $i]==$j){echo "checked";}}
     }
     echo "</div>";
   }
