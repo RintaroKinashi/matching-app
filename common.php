ad128393091306918fbe70d4a811d13f6c49b103
KinashiRintaro <nagisa624174@gmail.com>
2022-01-06 18:02:24 +0900

    [update] 質問8個に対応

--- common.php
+++ common.php
@@ -7,10 +7,10 @@ if (!isset($_SESSION["login"])) {
   header("Location: index.php");
   exit();
 }
-$dsn='mysql:dbname=EC;charset=utf8';
-$user='root';
-$password='';
-$dbh = new PDO($dsn,$user,$password, [
+$dsn = 'mysql:dbname=EC;charset=utf8';
+$user = 'root';
+$password = '';
+$dbh = new PDO($dsn, $user, $password, [
   // エラー発生時にエラーを投げる。（エラーコードのみ等ではなく）
   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
   // 結果セットに 返された際のカラム名で添字を付けた配列を返す。
@@ -18,26 +18,28 @@ $dbh = new PDO($dsn,$user,$password, [
 ]);
 
 // 問題数
-const NUMBER_OF_QUESTIONS = 4;
+const NUMBER_OF_QUESTIONS = 8;
 ?>
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>test</title>
-    <link rel="stylesheet" href="stylesheet.css">
-  </head>
-  <body>
-    <div class="header">
-      <div class="header-list">
-        <ul>
-          <li><a href="./profile.php">MyProfile</li>
-          <li><a href="./question.php">分析</a></li>
-          <li><a href="./matching.php">検索</a></li>
-          <li><a href="./notice.php">友達申請</a></li>
-          <li><a href="./contactform.php">お問い合わせ</a></li>
-          <li><a href="./logout.php">ログアウト</a></li>
-        </ul>
-      </div>
+
+<head>
+  <meta charset="utf-8">
+  <title>test</title>
+  <link rel="stylesheet" href="stylesheet.css">
+</head>
+
+<body>
+  <div class="header">
+    <div class="header-list">
+      <ul>
+        <li><a href="./profile.php">MyProfile</li>
+        <li><a href="./question.php">分析</a></li>
+        <li><a href="./matching.php">検索</a></li>
+        <li><a href="./notice.php">友達申請</a></li>
+        <li><a href="./contactform.php">お問い合わせ</a></li>
+        <li><a href="./logout.php">ログアウト</a></li>
+      </ul>
     </div>
+  </div>
--- question.php
+++ question.php
@@ -5,56 +5,59 @@ require 'common.php';
 $stmt1 = $dbh->prepare("SELECT * FROM r_answer WHERE UserID=:userID");
 $stmt1->bindParam(':userID', $_SESSION["login"]);
 $stmt1->execute();
-$result1 = $stmt1 -> fetch(PDO::FETCH_ASSOC);
+$result1 = $stmt1->fetch(PDO::FETCH_ASSOC);
 
-if(isset($_POST['q1'])){
-  if (empty($result1["UserID"])){
+if (isset($_POST['q1'])) {
+  if (empty($result1["UserID"])) {
     // 質問に未回答(IDが空)ならinsert
     $SQL = "INSERT INTO r_answer VALUES(:userID,";
-    for ($i=1;$i<NUMBER_OF_QUESTIONS;$i++){
-      $SQL .= ":q". $i . ",";
+    for ($i = 1; $i < NUMBER_OF_QUESTIONS; $i++) {
+      $SQL .= ":q" . $i . ",";
     }
-    $SQL .= ":q". NUMBER_OF_QUESTIONS . ");";
+    $SQL .= ":q" . NUMBER_OF_QUESTIONS . ");";
     echo $SQL;
     $stmt = $dbh->prepare($SQL);
-  }
-  else{
+  } else {
     $SQL = "UPDATE r_answer SET ";
-    for ($i=1;$i<NUMBER_OF_QUESTIONS;$i++){
-      $SQL .= "q". $i . " = :q". $i . ", ";
+    for ($i = 1; $i < NUMBER_OF_QUESTIONS; $i++) {
+      $SQL .= "q" . $i . " = :q" . $i . ", ";
     }
-    $SQL .= "q". NUMBER_OF_QUESTIONS . " = :q". NUMBER_OF_QUESTIONS . " WHERE UserID = :userID;";
+    $SQL .= "q" . NUMBER_OF_QUESTIONS . " = :q" . NUMBER_OF_QUESTIONS . " WHERE UserID = :userID;";
     echo $SQL;
     $stmt = $dbh->prepare($SQL);
   }
-    $stmt->bindParam(':userID', $_SESSION["login"]);
-    for ($i=1;$i<NUMBER_OF_QUESTIONS+1;$i++){
-      $stmt->bindParam(':q'. $i, $_POST['q'. $i]);
-    }
-    $stmt->execute();
-    header("Location: matching.php");
-    exit();
+  $stmt->bindParam(':userID', $_SESSION["login"]);
+  for ($i = 1; $i < NUMBER_OF_QUESTIONS + 1; $i++) {
+    $stmt->bindParam(':q' . $i, $_POST['q' . $i]);
+  }
+  $stmt->execute();
+  header("Location: matching.php");
+  exit();
 }
 ?>
 
 <form action="question.php" method="post">
-<?php
-  for ($i=1;$i<NUMBER_OF_QUESTIONS+1;$i++){
-  $stmtq = $dbh->prepare("SELECT * FROM m_question WHERE questionID=$i");
-  $stmtq->execute();
-  $resultq = $stmtq -> fetch(PDO::FETCH_ASSOC);
+  <?php
+  for ($i = 1; $i < NUMBER_OF_QUESTIONS + 1; $i++) {
+    $stmtq = $dbh->prepare("SELECT * FROM m_question WHERE questionID=$i");
+    $stmtq->execute();
+    $resultq = $stmtq->fetch(PDO::FETCH_ASSOC);
 
     echo "<div class='question'>";
     echo "<p>Q" . $i . "：" . $resultq["sentence"] . "</p>";
-    for ($j=0;$j<4;$j++){
+    for ($j = 0; $j < 4; $j++) {
       $k = "";
-      if(isset($result1['UserID']) && $result1['q'. $i]==$j){$k = "checked";}
-      echo "<input type='radio' name='q". $i ."' value='" . $j ."' ". $k .">" . $resultq["Choice".(string)($j+1)]; // if(!empty($result1["UserID"])){if($result1['q'. $i]==$j){echo "checked";}}
+      if (isset($result1['UserID']) && $result1['q' . $i] == $j) {
+        $k = "checked";
+      }
+      echo "<input type='radio' name='q" . $i . "' value='" . $j . "' " . $k . ">" . $resultq["Choice" . (string)($j + 1)]; // if(!empty($result1["UserID"])){if($result1['q'. $i]==$j){echo "checked";}}
+      echo "</br>";
     }
-    echo "</div>";
+    echo "</br></div>";
   }
-?>
-    <p><input type="submit" name="send" value="送信する"></p>
+  ?>
+  <p><input type="submit" name="send" value="送信する"></p>
 </form>
 </body>
+
 </html>
